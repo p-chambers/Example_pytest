3ed57a6981ce16ae3b02fc1fda796f9e61e53414
Paul Chambers <P.R.Chambers@soton.ac.uk>
2016-01-08 18:19:36 +0000

    Added conda location to Jenkins build file (conda was not located)

--- Jenkins_build.sh
+++ Jenkins_build.sh
@@ -2,7 +2,10 @@
 # @Author: pchambers
 # @Date:   2016-01-08 17:35:49
 # @Last Modified by:   pchambers
-# @Last Modified time: 2016-01-08 18:01:21
+# @Last Modified time: 2016-01-08 18:18:59
+
+# Tell the system where conda is located:
+export PATH="/home/pchambers/anaconda2/bin:$PATH"
 
 # Create the Conda test environment
 conda create --name jenkins_example_pytest python=2
