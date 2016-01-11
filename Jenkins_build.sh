ce49632999bd81d2de45d2404e6de4310d0d6b83
p-chambers <P.R.Chambers@soton.ac.uk>
2016-01-11 16:50:56 +0000

    Fixed jenkins build file - py.test is now added to the conda environment

--- Jenkins_build.sh
+++ Jenkins_build.sh
@@ -4,22 +4,14 @@
 # @Last Modified by:   pchambers
 # @Last Modified time: 2016-01-08 18:40:15
 
-# Tell the system where conda is: MOVED TO ENVIRONMENT VARIABLES IN JENKINS 
-# export PATH="/home/pchambers/anaconda2/bin:$PATH"
-
-# Check which python will be used
-which python
-
-# Create the Conda test environment
-conda create --name jenkins_example_pytest python=2
+# Create the Conda test environment (note that pytest is required, otherwise wrong
+# python is used and the module is not found!)
+conda create --name jenkins_example_pytest python=2 pytest
 source activate jenkins_example_pytest
-
-# Check which python will be used (should now be jenkins_home/.conda/ somewhere)
-which python
 
 python setup.py install
 py.test -v
 
 # Clean the Conda test environment
 source deactivate
-#conda env remove -n jenkins_example_pytest
+conda env remove -n jenkins_example_pytest
